353f5e57f3bc7b57791d041238ade0436b3b70e6
Jon Schneider <jschneider@gradle.com>
2019-12-01 12:34:46 +0000

    Polish formatting, spelling, fix concurrency issue on saving state of failed tests

--- src/main/java/org/gradle/plugins/testretry/RetryTestListener.java
+++ src/main/java/org/gradle/plugins/testretry/RetryTestListener.java
@@ -1,15 +1,30 @@
+/**
+ * Copyright 2019 Gradle, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gradle.plugins.testretry;
 
 import org.gradle.api.tasks.testing.TestDescriptor;
 import org.gradle.api.tasks.testing.TestListener;
 import org.gradle.api.tasks.testing.TestResult;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class RetryTestListener implements TestListener {
 
-    private List<TestDescriptor> failedTests = new ArrayList<TestDescriptor>();
+    private List<TestDescriptor> failedTests = new CopyOnWriteArrayList<>();
 
     @Override
     public void beforeSuite(TestDescriptor testDescriptor) {
