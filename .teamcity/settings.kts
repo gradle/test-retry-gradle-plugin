497fb85525bb3a84a9b8cd8ce4e9c18412e12f29
rene <rene@TeamCity>
2019-12-06 08:56:23 +0100

    TeamCity change in 'Gradle Plugins / Gradle Test Retry Plugin' project: Versioned settings configuration updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,9 +1,7 @@
-import jetbrains.buildServer.configs.kotlin.v2018_2.FailureAction
+import jetbrains.buildServer.configs.kotlin.v2018_2.*
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.gradle
-import jetbrains.buildServer.configs.kotlin.v2018_2.project
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.schedule
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.v2018_2.version
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -30,78 +28,162 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2019.1"
 
 project {
+
+    buildType(null_QuickFeedback)
+    buildType(null_CrossVersionTestLinuxJava18)
+    buildType(null_PublishSnapshot)
+    buildType(null_VerifyAll)
+
     params {
-        java8Home(Os.linux)
+        param("env.JAVA_HOME", "%linux.java8.oracle.64bit%")
         text("systemProp.org.gradle.internal.publish.checksums.insecure", "true")
     }
-
-    val quickFeedbackBuildType = buildType("Quick Feedback") {
-        steps {
-            gradle {
-                tasks = "clean build"
-                buildFile = ""
-            }
-        }
-    }
-    val crossVersionTestLinux = buildType("CrossVersionTest Linux - Java 1.8") {
-        steps {
-            gradle {
-                tasks = "clean testAll"
-                buildFile = ""
-                gradleParams = "-s $useGradleInternalScansServer"
-                param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
-            }
-        }
-
-        dependencies {
-            snapshot(quickFeedbackBuildType) {
-                onDependencyFailure = FailureAction.CANCEL
-                onDependencyCancel = FailureAction.CANCEL
-            }
-        }
-    }
-    val verifyAllBuildType = buildType("Verify all") {
-        triggers.vcs {}
-
-        dependencies {
-            snapshot(quickFeedbackBuildType) {
-                onDependencyFailure = FailureAction.CANCEL
-                onDependencyCancel = FailureAction.CANCEL
-            }
-            snapshot(crossVersionTestLinux) {
-                onDependencyFailure = FailureAction.CANCEL
-                onDependencyCancel = FailureAction.CANCEL
-            }
-        }
-    }
-    buildType("Publish Snapshot") {
-        description = "Publish Gradle Test Retry Plugin snapshot to Gradle's Artifactory repository"
-
-        steps {
-            gradle {
-                tasks = "clean devSnapshot publishPluginMavenPublicationToGradleBuildInternalRepository"
-                gradleParams = "-PartifactoryUsername=%ARTIFACTORY_USERNAME% -PartifactoryPassword=%ARTIFACTORY_PASSWORD% $useGradleInternalScansServer"
-                param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
-                buildFile = ""
-            }
-        }
-        dependencies {
-            snapshot(verifyAllBuildType) {
-                onDependencyFailure = FailureAction.CANCEL
-                onDependencyCancel = FailureAction.CANCEL
-            }
-        }
-
-        // publish a nightly snapshot
-        triggers.schedule {
-                schedulingPolicy = daily {
-                    hour = 2
-                }
-                branchFilter = "+:refs/head/master"
-                triggerBuild = always()
-                withPendingChangesOnly = false
-        }
-
-    }
-
+    buildTypesOrder = arrayListOf(null_QuickFeedback, null_CrossVersionTestLinuxJava18, null_VerifyAll, null_PublishSnapshot)
 }
+
+object null_CrossVersionTestLinuxJava18 : BuildType({
+    name = "CrossVersionTest Linux - Java 1.8"
+
+    artifactRules = "build/reports/** => reports"
+
+    params {
+        param("env.JAVA_HOME", "%linux.java8.oracle.64bit%")
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+
+        checkoutMode = CheckoutMode.ON_SERVER
+    }
+
+    steps {
+        gradle {
+            tasks = "clean testAll"
+            buildFile = ""
+            gradleParams = "-s -I gradle/init-scripts/build-scan.init.gradle.kts"
+            param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
+        }
+    }
+
+    dependencies {
+        snapshot(null_QuickFeedback) {
+            onDependencyFailure = FailureAction.CANCEL
+            onDependencyCancel = FailureAction.CANCEL
+        }
+    }
+
+    requirements {
+        contains("teamcity.agent.jvm.os.name", "Linux")
+    }
+})
+
+object null_PublishSnapshot : BuildType({
+    name = "Publish Snapshot"
+    description = "Publish Gradle Test Retry Plugin snapshot to Gradle's Artifactory repository"
+
+    artifactRules = "build/reports/** => reports"
+
+    params {
+        param("env.JAVA_HOME", "%linux.java8.oracle.64bit%")
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+
+        checkoutMode = CheckoutMode.ON_SERVER
+    }
+
+    steps {
+        gradle {
+            tasks = "clean devSnapshot publishPluginMavenPublicationToGradleBuildInternalRepository"
+            buildFile = ""
+            gradleParams = "-PartifactoryUsername=%ARTIFACTORY_USERNAME% -PartifactoryPassword=%ARTIFACTORY_PASSWORD% -I gradle/init-scripts/build-scan.init.gradle.kts"
+            param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
+        }
+    }
+
+    triggers {
+        schedule {
+            schedulingPolicy = daily {
+                hour = 2
+            }
+            branchFilter = "+:refs/head/master"
+            triggerBuild = always()
+            withPendingChangesOnly = false
+        }
+    }
+
+    dependencies {
+        snapshot(null_VerifyAll) {
+            onDependencyFailure = FailureAction.CANCEL
+            onDependencyCancel = FailureAction.CANCEL
+        }
+    }
+
+    requirements {
+        contains("teamcity.agent.jvm.os.name", "Linux")
+    }
+})
+
+object null_QuickFeedback : BuildType({
+    name = "Quick Feedback"
+
+    artifactRules = "build/reports/** => reports"
+
+    params {
+        param("env.JAVA_HOME", "%linux.java8.oracle.64bit%")
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+
+        checkoutMode = CheckoutMode.ON_SERVER
+    }
+
+    steps {
+        gradle {
+            tasks = "clean build"
+            buildFile = ""
+        }
+    }
+
+    requirements {
+        contains("teamcity.agent.jvm.os.name", "Linux")
+    }
+})
+
+object null_VerifyAll : BuildType({
+    name = "Verify all"
+
+    artifactRules = "build/reports/** => reports"
+
+    params {
+        param("env.JAVA_HOME", "%linux.java8.oracle.64bit%")
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+
+        checkoutMode = CheckoutMode.ON_SERVER
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+
+    dependencies {
+        snapshot(null_CrossVersionTestLinuxJava18) {
+            onDependencyFailure = FailureAction.CANCEL
+            onDependencyCancel = FailureAction.CANCEL
+        }
+        snapshot(null_QuickFeedback) {
+            onDependencyFailure = FailureAction.CANCEL
+            onDependencyCancel = FailureAction.CANCEL
+        }
+    }
+
+    requirements {
+        contains("teamcity.agent.jvm.os.name", "Linux")
+    }
+})
