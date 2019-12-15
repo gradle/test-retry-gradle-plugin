02089e414f8f08999a1e92cfa412820027bfd31b
Luke Daley <ld@ldaley.com>
2019-12-16 09:11:54 +1000

    Format

--- buildSrc/src/main/groovy/org/gradle/plugins/build/GradleVersionData.groovy
+++ buildSrc/src/main/groovy/org/gradle/plugins/build/GradleVersionData.groovy
@@ -1,6 +1,7 @@
 package org.gradle.plugins.build
 
-import groovy.json.JsonSlurper;
+import groovy.json.JsonSlurper
+
 class GradleVersionData {
 
     static List<String> getNightlyVersions() {
@@ -18,23 +19,23 @@ class GradleVersionData {
 
     static List<String> getGradleReleases() {
         def allReleases = new JsonSlurper().parse(new URL("https://services.gradle.org/versions/all"))
-        return allReleases.findAll {!it.nigthly && !it.snapshot }       // filter out snapshots and nightlies
-            .findAll{!it.rcFor || it.activeRc}                          // filter out inactive rcs
-            .inject([]) {releasesToTest, currentEntry ->   // filter out obsolete milestones
-                if(currentEntry.milestoneFor && (releasesToTest.find{processedEntry -> processedEntry.milestoneFor == currentEntry.milestoneFor || processedEntry.version == currentEntry.milestoneFor})) {
-                    return releasesToTest
-                }
-                return releasesToTest + currentEntry
-            }
-            .collect{it.version}                                       // we're only interested in the verison
-            .findAll {((it.substring(0, 1)) as int) >=5 }              // only 5.0 and above
-            .inject([]) {releasesToTest, currentEntry ->  // only test against latest patch versions
-                if(!releasesToTest.any{it.startsWith(currentEntry.substring(0, 3))}) {
+        return allReleases.findAll { !it.nigthly && !it.snapshot }       // filter out snapshots and nightlies
+                .findAll { !it.rcFor || it.activeRc }                          // filter out inactive rcs
+                .inject([]) { releasesToTest, currentEntry ->   // filter out obsolete milestones
+                    if (currentEntry.milestoneFor && (releasesToTest.find { processedEntry -> processedEntry.milestoneFor == currentEntry.milestoneFor || processedEntry.version == currentEntry.milestoneFor })) {
+                        return releasesToTest
+                    }
                     return releasesToTest + currentEntry
-                } else {
-                    return releasesToTest
                 }
-            }
+                .collect { it.version }                                       // we're only interested in the verison
+                .findAll { ((it.substring(0, 1)) as int) >= 5 }              // only 5.0 and above
+                .inject([]) { releasesToTest, currentEntry ->  // only test against latest patch versions
+                    if (!releasesToTest.any { it.startsWith(currentEntry.substring(0, 3)) }) {
+                        return releasesToTest + currentEntry
+                    } else {
+                        return releasesToTest
+                    }
+                }
     }
 
 }
