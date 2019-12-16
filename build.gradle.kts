b2088f14356345b537aadcb9f62544429241a7ec
Luke Daley <ld@ldaley.com>
2019-12-16 16:30:45 +1000

    Add build scan link to TC build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,13 @@ plugins {
     id("nebula.release") version "13.2.1"
 }
 
+buildScan {
+    val buildUrl = System.getenv("BUILD_URL") ?: ""
+    if (buildUrl.isNotBlank()) {
+        link("Build URL", buildUrl)
+    }
+}
+
 group = "org.gradle"
 description = "Mitigate flaky tests by retrying tests when they fail"
 
---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("com.gradle.enterprise") version "3.1"
+}
+
+rootProject.name = "test-retry-plugin"
+
+include("plugin")
+include("sample-tests")
