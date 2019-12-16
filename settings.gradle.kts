674c2202ca20450b1f720644323c1a28b1abff39
Rene Groeschke <rene@breskeby.com>
2019-12-16 09:33:42 +0100

    Enable build cache
    - use remote on ci
    - use local for local builds
    
    - Fix #9

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.gradle.enterprise") version "3.1"
 }
 
+apply(from = "gradle/build-cache-configuration.settings.gradle.kts")
+
 rootProject.name = "test-retry-plugin"
 
 include("plugin")
