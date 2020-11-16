c47ef3ea02c39990802548488d4bf8a4c1c3d7ea
Luke Daley <ld@ldaley.com>
2020-11-16 11:57:51 +1000

    Update Gradle version and add magic go-fast flags

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,15 @@
-plugins {
-    id("com.gradle.enterprise")  version "3.4"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven { url = uri("https://repo.gradle.org/gradle/libs-releases") }
+    }
 }
+
+plugins {
+    id("com.gradle.enterprise").version("3.5")
+    id("com.gradle.enterprise.gradle-enterprise-conventions-plugin").version("0.7.1")
+}
+
 gradleEnterprise {
     buildScan {
         val buildUrl = System.getenv("BUILD_URL") ?: ""
