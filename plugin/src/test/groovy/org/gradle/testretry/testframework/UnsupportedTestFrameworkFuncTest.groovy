5fc24f29942e8f3db22f671de23a8d1ff98897bc
Daniel Clausen <dclausen@gradle.com>
2022-10-18 18:42:32 +0200

    Fix test
    The latest Gradle 8.0 nightly no longer allows setting the test framework
    on the test task after it is read.

--- plugin/src/test/groovy/org/gradle/testretry/testframework/UnsupportedTestFrameworkFuncTest.groovy
+++ plugin/src/test/groovy/org/gradle/testretry/testframework/UnsupportedTestFrameworkFuncTest.groovy
@@ -17,24 +17,35 @@ package org.gradle.testretry.testframework
 
 import org.gradle.api.internal.tasks.testing.TestFramework
 import org.gradle.testretry.AbstractFrameworkFuncTest
+import org.gradle.util.GradleVersion
 
 class UnsupportedTestFrameworkFuncTest extends AbstractFrameworkFuncTest {
 
+    private static final GradleVersion GRADLE_7_999 = GradleVersion.version("7.999")
+
     def "logs warning if test framework is unsupported"(String gradleVersion) {
         given:
+        def gradle8OrAbove = GradleVersion.version(gradleVersion) > GRADLE_7_999
+
         buildFile << """
             test.retry.maxRetries = 2
-            
+
             class CustomTestFramework implements $TestFramework.name {
-                @Delegate 
+                @Delegate
                 private final $TestFramework.name delegate
 
-                CustomTestFramework($TestFramework.name delegate) {
-                    this.delegate = delegate
+                CustomTestFramework(org.gradle.api.tasks.testing.Test testTask) {
+                    def testFilter = new org.gradle.api.internal.tasks.testing.filter.DefaultTestFilter()
+
+                    this.delegate = new org.gradle.api.internal.tasks.testing.junit.JUnitTestFramework(
+                        testTask,
+                        testFilter,
+                        ${gradle8OrAbove ? 'true' : ''}
+                    )
                 }
             }
-            
-            test.useTestFramework(new CustomTestFramework(test.testFramework))
+
+            test.useTestFramework(new CustomTestFramework(test))
         """
 
         successfulTest()
