b292e8d5ceb35d987b336a70771b7cee23723a69
Daniel Clausen <dclausen@gradle.com>
2022-11-10 17:10:30 +0100

    Replace call of constructor for older Gradle versions with reflection
    This way, the old constructors can be removed from the Gradle 8 code base.
    
    Signed-off-by: Daniel Clausen <dclausen@gradle.com>

--- plugin/src/main/java/org/gradle/testretry/internal/executer/JvmTestExecutionSpecFactory.java
+++ plugin/src/main/java/org/gradle/testretry/internal/executer/JvmTestExecutionSpecFactory.java
@@ -15,9 +15,15 @@
  */
 package org.gradle.testretry.internal.executer;
 
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.file.FileTree;
 import org.gradle.api.internal.tasks.testing.JvmTestExecutionSpec;
 import org.gradle.api.internal.tasks.testing.TestFramework;
+import org.gradle.process.JavaForkOptions;
 import org.gradle.util.GradleVersion;
+
+import java.lang.reflect.Constructor;
+import java.util.Set;
 
 enum JvmTestExecutionSpecFactory {
 
@@ -31,41 +37,80 @@ enum JvmTestExecutionSpecFactory {
     FACTORY_FOR_GRADLE_OLDER_THAN_V8 {
         @Override
         JvmTestExecutionSpec createExecutionSpec(TestFramework testFramework, JvmTestExecutionSpec source) {
-            // This constructor is in Gradle 6.4+
-            return new JvmTestExecutionSpec(
-                testFramework,
-                source.getClasspath(),
-                source.getModulePath(),
-                source.getCandidateClassFiles(),
-                source.isScanForTestClasses(),
-                source.getTestClassesDirs(),
-                source.getPath(),
-                source.getIdentityPath(),
-                source.getForkEvery(),
-                source.getJavaForkOptions(),
-                source.getMaxParallelForks(),
-                source.getPreviousFailedTestClasses()
-            );
+            try {
+                Class<?> clazz = JvmTestExecutionSpec.class;
+                // This constructor is available in Gradle 6.4+
+                Constructor<?> constructor = clazz.getConstructor(
+                    TestFramework.class,
+                    Iterable.class,
+                    Iterable.class,
+                    FileTree.class,
+                    boolean.class,
+                    FileCollection.class,
+                    String.class,
+                    org.gradle.util.Path.class,
+                    long.class,
+                    JavaForkOptions.class,
+                    int.class,
+                    Set.class
+                );
+
+                return (JvmTestExecutionSpec) constructor.newInstance(
+                    testFramework,
+                    source.getClasspath(),
+                    source.getModulePath(),
+                    source.getCandidateClassFiles(),
+                    source.isScanForTestClasses(),
+                    source.getTestClassesDirs(),
+                    source.getPath(),
+                    source.getIdentityPath(),
+                    source.getForkEvery(),
+                    source.getJavaForkOptions(),
+                    source.getMaxParallelForks(),
+                    source.getPreviousFailedTestClasses()
+                );
+            } catch (ReflectiveOperationException e) {
+                throw new RuntimeException(e);
+            }
         }
     },
 
     FACTORY_FOR_GRADLE_OLDER_THAN_V6_4 {
         @Override
         JvmTestExecutionSpec createExecutionSpec(TestFramework testFramework, JvmTestExecutionSpec source) {
-            // This constructor is in Gradle 4.7+
-            return new JvmTestExecutionSpec(
-                testFramework,
-                source.getClasspath(),
-                source.getCandidateClassFiles(),
-                source.isScanForTestClasses(),
-                source.getTestClassesDirs(),
-                source.getPath(),
-                source.getIdentityPath(),
-                source.getForkEvery(),
-                source.getJavaForkOptions(),
-                source.getMaxParallelForks(),
-                source.getPreviousFailedTestClasses()
-            );
+            try {
+                Class<?> clazz = JvmTestExecutionSpec.class;
+                // This constructor is available in Gradle 4.7+
+                Constructor<?> constructor = clazz.getConstructor(
+                    TestFramework.class,
+                    Iterable.class,
+                    FileTree.class,
+                    boolean.class,
+                    FileCollection.class,
+                    String.class,
+                    org.gradle.util.Path.class,
+                    long.class,
+                    JavaForkOptions.class,
+                    int.class,
+                    Set.class
+                );
+
+                return (JvmTestExecutionSpec) constructor.newInstance(
+                    testFramework,
+                    source.getClasspath(),
+                    source.getCandidateClassFiles(),
+                    source.isScanForTestClasses(),
+                    source.getTestClassesDirs(),
+                    source.getPath(),
+                    source.getIdentityPath(),
+                    source.getForkEvery(),
+                    source.getJavaForkOptions(),
+                    source.getMaxParallelForks(),
+                    source.getPreviousFailedTestClasses()
+                );
+            } catch (ReflectiveOperationException e) {
+                throw new RuntimeException(e);
+            }
 
         }
     };
