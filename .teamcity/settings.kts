91b9d5940a44dd7576ae28b9b138b31e0d504548
Jean Gauthier <jgauthier@gradle.com>
2023-02-01 15:29:48 +0100

    Test trigger
    Signed-off-by: Jean Gauthier <jgauthier@gradle.com>

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -129,7 +129,7 @@ project {
             triggerRules = projectTriggerRules
             schedulingPolicy = daily {
                 hour = 15
-                minute = 31
+                minute = 32
             }
 //            branchFilter = "+:<default>"
             branchFilter = "+:refs/head/jgauthier/20027"
