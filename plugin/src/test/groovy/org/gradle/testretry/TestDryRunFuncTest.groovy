a1bfefb5fe16503d524bef379af21403b3cb2ea0
Jean Gauthier <jgauthier@gradle.com>
2023-07-18 13:51:15 +0200

    Add tests for test-dry-run set via JUnit Platform system property
    Signed-off-by: Jean Gauthier <jgauthier@gradle.com>

--- plugin/src/test/groovy/org/gradle/testretry/TestDryRunFuncTest.groovy
+++ plugin/src/test/groovy/org/gradle/testretry/TestDryRunFuncTest.groovy
@@ -21,12 +21,22 @@ import org.gradle.util.GradleVersion
 class TestDryRunFuncTest extends AbstractGeneralPluginFuncTest {
 
     private static final GradleVersion GRADLE_8_3 = GradleVersion.version("8.3")
+    private static final String MIN_JUPITER_VERSION_DRY_RUN = "5.10.0-RC1"
+    private static final String MIN_PLATFORM_VERSION_DRY_RUN = "1.10.0-RC1"
+
+    @Override
+    protected String buildConfiguration() {
+        return """dependencies {
+            testImplementation 'org.junit.jupiter:junit-jupiter:$MIN_JUPITER_VERSION_DRY_RUN'
+            testRuntimeOnly 'org.junit.platform:junit-platform-launcher:$MIN_PLATFORM_VERSION_DRY_RUN'
+        }"""
+    }
 
     def "emits skipped test method events if dryRun = true and retry plugin is enabled"() {
         given:
         def gradle83OrAbove = GradleVersion.version(gradleVersion) >= GRADLE_8_3
         setupTest(gradle83OrAbove, true)
-        successfulTest()
+        successfulJUnit5Test()
 
         when:
         def result = gradleRunner(gradleVersion).build()
@@ -42,10 +52,26 @@ class TestDryRunFuncTest extends AbstractGeneralPluginFuncTest {
         given:
         def gradle83OrAbove = GradleVersion.version(gradleVersion) >= GRADLE_8_3
         setupTest(gradle83OrAbove, false)
-        successfulTest()
+        successfulJUnit5Test()
 
         when:
         def result = gradle83OrAbove ? gradleRunner(gradleVersion,  'test', '-S', "--test-dry-run").build() : gradleRunner(gradleVersion).build()
+
+        then:
+        gradle83OrAbove ? methodSkipped(result) : methodPassed(result)
+
+        where:
+        gradleVersion << GRADLE_VERSIONS_UNDER_TEST
+    }
+
+    def "emits skipped test method events, if dryRun is set to true via system properties and retry plugin is enabled"() {
+        given:
+        def gradle83OrAbove = GradleVersion.version(gradleVersion) >= GRADLE_8_3
+        setupTest(gradle83OrAbove, false, Optional.of(true))
+        successfulJUnit5Test()
+
+        when:
+        def result = gradleRunner(gradleVersion).build()
 
         then:
         gradle83OrAbove ? methodSkipped(result) : methodPassed(result)
@@ -58,10 +84,26 @@ class TestDryRunFuncTest extends AbstractGeneralPluginFuncTest {
         given:
         def gradle83OrAbove = GradleVersion.version(gradleVersion) >= GRADLE_8_3
         setupTest(gradle83OrAbove, false)
-        successfulTest()
+        successfulJUnit5Test()
 
         when:
         def result = gradle83OrAbove ? gradleRunner(gradleVersion,  'test', '-S', "--no-test-dry-run").build() : gradleRunner(gradleVersion).build()
+
+        then:
+        methodPassed(result)
+
+        where:
+        gradleVersion << GRADLE_VERSIONS_UNDER_TEST
+    }
+
+    def "does not emit skipped test method events, if dryRun is set to false via system properties and retry plugin is enabled"() {
+        given:
+        def gradle83OrAbove = GradleVersion.version(gradleVersion) >= GRADLE_8_3
+        setupTest(gradle83OrAbove, false, Optional.of(false))
+        successfulJUnit5Test()
+
+        when:
+        def result = gradleRunner(gradleVersion).build()
 
         then:
         methodPassed(result)
@@ -74,7 +116,7 @@ class TestDryRunFuncTest extends AbstractGeneralPluginFuncTest {
         given:
         def gradle83OrAbove = GradleVersion.version(gradleVersion) >= GRADLE_8_3
         setupTest(gradle83OrAbove, false)
-        successfulTest()
+        successfulJUnit5Test()
 
         when:
         def result = gradleRunner(gradleVersion).build()
@@ -87,12 +129,29 @@ class TestDryRunFuncTest extends AbstractGeneralPluginFuncTest {
     }
 
     private void setupTest(boolean gradle83OrAbove, boolean withTestDryRun) {
+        setupTest(gradle83OrAbove, withTestDryRun, Optional.empty())
+    }
+
+    private void setupTest(boolean gradle83OrAbove, boolean withTestDryRun, Optional<Boolean> withSysPropDryRun) {
         buildFile << """
             test {
+                useJUnitPlatform()
                 ${gradle83OrAbove && withTestDryRun ? "dryRun = true" : ""}
+                ${withSysPropDryRun.map { it -> gradle83OrAbove && it ? "systemProperty('junit.platform.execution.dryRun.enabled', $it)" : "" }.orElse("")}
                 retry {
                     maxRetries = 1
                 }
+            }
+        """
+    }
+
+    private void successfulJUnit5Test() {
+        writeTestSource """
+            package acme;
+
+            public class SuccessfulTests {
+                @org.junit.jupiter.api.Test
+                public void successTest() {}
             }
         """
     }
