001f67857902146c6264971ba3b2fc6877e0f6a5
Pavlo Shevchenko <pshevchenko@gradle.com>
2024-10-14 14:41:48 +0200

    Update sample to not trigger warning when using Gradle's automatic loading of test framework dependencies
    Signed-off-by: Pavlo Shevchenko <pshevchenko@gradle.com>

--- samples/sample-junit5/build.gradle.kts
+++ samples/sample-junit5/build.gradle.kts
@@ -9,9 +9,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.2")
+    testImplementation(platform("org.junit:junit-bom:5.11.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
