80cdbc84686ecbd17dcb76b40377bca06142d3c8
bot-gh-dv-shared <bot-gh-dv-shared+renovate@gradle.com>
2024-10-14 11:44:28 +0200

    [Renovate Bot] Update junit5 monorepo to v5.11.2 (#328)
    Signed-off-by: Renovate Bot for Gradle <bot-gh-dv-shared+renovate@gradle.com>

--- samples/sample-junit5/build.gradle.kts
+++ samples/sample-junit5/build.gradle.kts
@@ -9,9 +9,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.2")
 }
 
 tasks.test {
