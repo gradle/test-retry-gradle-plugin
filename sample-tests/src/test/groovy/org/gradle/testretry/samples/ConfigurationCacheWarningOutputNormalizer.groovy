c4ee038aab3e9ed83c234751f6a9711b1b95b627
Pavlo Shevchenko <pshevchenko@gradle.com>
2025-01-02 10:21:21 +0100

    Update samples test to check for a new CC warning
    Signed-off-by: Pavlo Shevchenko <pshevchenko@gradle.com>

--- sample-tests/src/test/groovy/org/gradle/testretry/samples/ConfigurationCacheWarningOutputNormalizer.groovy
+++ sample-tests/src/test/groovy/org/gradle/testretry/samples/ConfigurationCacheWarningOutputNormalizer.groovy
@@ -7,5 +7,7 @@ class ConfigurationCacheWarningOutputNormalizer implements OutputNormalizer {
     @Override
     String normalize(String commandOutput, ExecutionMetadata executionMetadata) {
         return commandOutput.replaceAll(".*Test.getClassLoaderCache\\(\\) method has been deprecated.*\\R", "")
+            .replaceAll(".*Executing Gradle on JVM versions 16 and lower has been deprecated.*\\R", "")
+            .replaceAll(".*Problems report is available at.*\\R.*\\R", "")
     }
 }
