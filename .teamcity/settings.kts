76769c22de29e2585cfdaadccf1ce0c5584df138
Daniel Clausen <dclausen@gradle.com>
2025-02-05 16:38:12 +0100

    Update TeamCity Kotlin DSL to 2024.12
    Signed-off-by: Daniel Clausen <dclausen@gradle.com>

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,12 +1,12 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.BuildTypeSettings.Type.COMPOSITE
-import jetbrains.buildServer.configs.kotlin.v2019_2.FailureAction
-import jetbrains.buildServer.configs.kotlin.v2019_2.ParameterDisplay.NORMAL
-import jetbrains.buildServer.configs.kotlin.v2019_2.ParameterDisplay.PROMPT
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
-import jetbrains.buildServer.configs.kotlin.v2019_2.project
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.schedule
-import jetbrains.buildServer.configs.kotlin.v2019_2.version
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.BuildTypeSettings.Type.COMPOSITE
+import jetbrains.buildServer.configs.kotlin.FailureAction
+import jetbrains.buildServer.configs.kotlin.ParameterDisplay.NORMAL
+import jetbrains.buildServer.configs.kotlin.ParameterDisplay.PROMPT
+import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.project
+import jetbrains.buildServer.configs.kotlin.triggers.schedule
+import jetbrains.buildServer.configs.kotlin.version
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -30,7 +30,7 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 'Debug' option is available in the context menu for the task.
 */
 
-version = "2021.1"
+version = "2024.12"
 
 project {
     params {
