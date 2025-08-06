c59586f9afb78008aa712d1ff92c38a39c8a39de
Pavlo Shevchenko <pshevchenko@gradle.com>
2025-08-06 11:34:12 +0200

    Verify that the published artifact was compiled with Java 8
    Previous check was verifying that the `releaseCheck` task was running with Java 8, not necessarily that the published artifact was compiled with Java 8. By default, tasks will use Java 21 as it is the default JDK on our CI agents, and only compilation tasks will use 8 through Gradle toolchains.
    
    Signed-off-by: Pavlo Shevchenko <pshevchenko@gradle.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 plugins {
     id("nebula.release") version "20.2.0"
     id("org.gradle.wrapper-upgrade") version "0.12"
@@ -17,10 +20,28 @@ evaluationDependsOn("plugin")
 
 val publishPlugins = tasks.findByPath(":plugin:publishPlugins")
 
+fun runCommand(vararg command: String): String {
+    val process = ProcessBuilder(command.toList()).start()
+    val reader = BufferedReader(InputStreamReader(process.inputStream))
+    val output = StringBuilder()
+    var line: String?
+    while (reader.readLine().also { line = it } != null) {
+        output.append(line).append("\n")
+    }
+    process.waitFor()
+    return output.toString()
+}
+
+val jar = tasks.getByPath(":plugin:shadowJar")
 tasks.named("releaseCheck") {
+    inputs.files(jar.outputs.files)
     doFirst {
-        if (!JavaVersion.current().isJava8) {
-            throw GradleException("Plugin releases should use Java 8, but used ${JavaVersion.current()} instead.")
+        val jarPath = jar.outputs.files.singleFile.path
+        val classFileInfo = runCommand("javap", "-cp", jarPath, "-v", "org.gradle.testretry.TestRetryPlugin")
+        val classFileVersion = requireNotNull("major version: (\\d+)".toRegex().find(classFileInfo)).groupValues.last().toInt()
+        val javaVersionForCompilation = JavaVersion.forClassVersion(classFileVersion)
+        if (!javaVersionForCompilation.isJava8) {
+            throw GradleException("Plugin releases should use Java 8, but was $javaVersionForCompilation")
         }
     }
 }
