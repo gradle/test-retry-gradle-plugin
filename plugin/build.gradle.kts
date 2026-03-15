22d2e309bd56464bd7ad143183dce24a4e2cce70
bot-gh-dv-shared <bot-gh-dv-shared+renovate@gradle.com>
2026-03-15 06:22:55 +0100

    [Renovate Bot] Update plugin com.gradleup.shadow to v9.4.0 (#507)
    Signed-off-by: Renovate Bot for Gradle <bot-gh-dv-shared+renovate@gradle.com>

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
     signing
     id("com.gradle.plugin-publish") version "2.1.0"
     id("com.github.hierynomus.license") version "0.16.1"
-    id("com.gradleup.shadow") version "9.3.2"
+    id("com.gradleup.shadow") version "9.4.0"
 }
 
 group = "org.gradle"
